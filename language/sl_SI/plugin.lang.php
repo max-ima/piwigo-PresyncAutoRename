88cbdc6587566c43117561a02a867867cbea75a4
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-16 23:56:33 +0100

    sl_SI
    Slovenian language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Slovenian
+ */
+
+$lang['Directory '] = 'Imenik ';
+$lang['renamed to '] = 'preimenovan v ';
+$lang['to be renamed to '] = 'preimenovati v ';
+$lang['File '] = 'Datoteka ';
+$lang['Autorename directories and filenames in gallery '] = 'Samodejno preimenujte imenike in imena datotek v galeriji ';
+$lang['Error list'] = 'Seznam napak';
+$lang['Errors caption'] = 'Napis o napakah';
+$lang['Rename process results'] = 'Preimenujte rezultate postopka';
+$lang['Choose an option'] = 'Izberite možnost';
+$lang['Fix invalid names for '] = 'Popravite neveljavna imena za ';
+$lang['only directories'] = 'samo imeniki';
+$lang['directories and files'] = 'imeniki in datoteke';
+$lang['Simulation'] = 'Simulacija';
+$lang['Simulation mode (no change will be made on the filesystem)'] = 'Simulacijski način (datotečni sistem ne bo spremenjen)';
+$lang['reduce to single existing albums'] = 'zmanjšati na posamezne obstoječe albume';
+$lang['Search in sub-directories'] = 'Išči v podimenikih';
+$lang['Submit'] = 'Pošlji';
+$lang['Reset'] = 'Ponastavi';
+$lang['The scan did not find any file which do not conform to required naming pattern'] = 'Pregled ni našel nobene datoteke, ki ne ustreza zahtevanemu vzorcu poimenovanja';
+$lang['presyncautorename_intro'] = 'Ta vtičnik brska po vaši galeriji in samodejno preimenuje vse neveljavne mape in imena datotek. <br/>Neveljavni znaki se nadomestijo s podčrtaji. V primeru podvojenih imen datotek so datoteke s pripono z inkrementalno številko.';
+?>
